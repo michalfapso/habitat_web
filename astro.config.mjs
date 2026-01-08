1d3e40ebd46abd93b3555fbd206dbf99b338d16e
Michal Fapso <michal.fapso@gmail.com>
2026-01-08 21:48:00 +0100

    language switcher is hidden by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'http://localhost:4321',
     compressHTML: false, // Just to see the "View page source" indented properly
@@ -12,4 +11,10 @@ export default defineConfig({
             prefixDefaultLocale: false,
         },
     },
+    vite: {
+        define: {
+            SHOW_LANGUAGE_SWITCHER: false,
+        },
+    },
 });
+
