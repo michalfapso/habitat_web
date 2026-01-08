cf294e5d8251d9e8f375414b5bf1f6c1cfd769bb
jakubmikula-habitat <jakub.mikula@habitat.sk>
2026-01-08 11:58:31 +0100

    Update cz.ts

--- src/i18n/cz.ts
+++ src/i18n/cz.ts
@@ -28,7 +28,7 @@ export default {
   "services.mainTitle":
     'Žádná prázdná architektonická gesta. <br class="show-for-xlarge">Naše domy jsou efektivní a&nbsp;smysluplné.',
   "services.subTitle": "Naše služby",
-  "projects.title": "Projekty",
+  "projects.title": "Projekty pasivních domů",
   "projects.all": "Všechny projekty",
   "projects.back": "Zpět na projekty",
   "projects.about": "O projektu",
